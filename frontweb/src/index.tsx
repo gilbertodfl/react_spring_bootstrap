a8fdc38ffdc76cc464135d2e226cb256505b0583
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-09-16 14:09:38 -0300

    Bootstrap theme customization - scss

--- frontweb/src/index.tsx
+++ frontweb/src/index.tsx
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-/* Aqui habilitamos o uso do bootstrap depois de termos executado
-     yarn add bootstrap 
-  no comando de getLineAndCharacterOfPosition-->
-*/
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap.css'
-
-import { getLineAndCharacterOfPosition } from 'typescript';
 
 ReactDOM.render(
   <React.StrictMode>
