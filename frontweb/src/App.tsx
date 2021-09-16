311d8078e47d5c2200da867ce766c35b2c831c69
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-09-16 16:19:26 -0300

    Implement css in Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,19 @@
 //import React from 'react';
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
+
+
 // pagina principal
 function App() {
+
   return (
- <h1>Hello World!!</h1>  );
+    <>
+    <Navbar />
+     <h1>Hello World!!</h1>;
+     </>
+     );
+ 
 }
 
 export default App;
