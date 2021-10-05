d6a375cfa75d201982b5e3132c732051b4e42ca6
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-10-05 19:25:30 -0300

    Props ProductCard, ProductPrice

---
+++ frontweb/src/types/product.ts
@@ -0,0 +1,15 @@
+import { Category } from "./category"
+
+export type Product  = {
+
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imgUrl: string;
+    date: string;   
+
+    categories: Category[]
+    
+}
+
