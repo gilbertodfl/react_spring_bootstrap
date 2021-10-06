3d8c7cfd2b1042a209b6cdb8eeec1b35e387d0d1
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-10-05 21:31:01 -0300

    html productdetails

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,39 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+
+
 const ProductDetails =  () => {
 return(
-<h1>detail</h1>
+<div className="product-details-container">
+    <div className="product-details-card">
+        <div className="go-back-container">
+            <ArrowIcon />
+            <h2>voltar</h2>
+        </div>
+
+    </div>
+    <div className="row">
+        <div className="col-xl-6">
+            <div className="img-container">
+                <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="nome do produto" /> 
+
+            </div>
+
+            <div className="name-price-container">
+                    <h1>Nome Produto</h1>
+                    <ProductPrice price={ 1234.56 } />
+
+
+            </div>
+        </div>
+        <div className="col-xl-6">
+         <div className="description-container">
+             <h2>Descrição do Produto</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, cupiditate.</p>
+         </div>
+        </div>
+    </div>
+</div>
 
 )
 }
